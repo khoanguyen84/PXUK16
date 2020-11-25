3017b47bca5658ba132ef4794f5e56d08f1c45f0
ntanh <70925610+tuananh200599@users.noreply.github.com>
2020-11-25 19:32:39 +0700

    Update

---
+++ PXUK16.API/Database/26112020/DataChanged.sql
@@ -0,0 +1,16 @@
+﻿USE [PXUK16DB]
+GO
+SET IDENTITY_INSERT [dbo].[Manafactory] ON 
+GO
+INSERT [dbo].[Manafactory] ([ManufactoryId], [Name], [IsDeleted]) VALUES (1, N'Lenovo', 0)
+GO
+INSERT [dbo].[Manafactory] ([ManufactoryId], [Name], [IsDeleted]) VALUES (2, N'Dell', 0)
+GO
+INSERT [dbo].[Manafactory] ([ManufactoryId], [Name], [IsDeleted]) VALUES (3, N'Apple', 0)
+GO
+INSERT [dbo].[Manafactory] ([ManufactoryId], [Name], [IsDeleted]) VALUES (4, N'Hp', 0)
+GO
+INSERT [dbo].[Manafactory] ([ManufactoryId], [Name], [IsDeleted]) VALUES (5, N'Asus', 0)
+GO
+SET IDENTITY_INSERT [dbo].[Manafactory] OFF
+GO
