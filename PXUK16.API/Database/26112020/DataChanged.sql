ac958cfce87eba9ec007a7c9b476d044988f25b7
Thanh Dat <70925557+hntdahta@users.noreply.github.com>
2020-11-25 18:50:59 +0700

    Update DataChanged.sql

--- PXUK16.API/Database/26112020/DataChanged.sql
+++ PXUK16.API/Database/26112020/DataChanged.sql
@@ -2,9 +2,9 @@
 GO
 SET IDENTITY_INSERT [dbo].[Manafactory] ON 
 
-INSERT [dbo].[Manafactory] ([ManufactoryId], [Name], [IsDeleted]) VALUES (10, N'Dat', 0)
-INSERT [dbo].[Manafactory] ([ManufactoryId], [Name], [IsDeleted]) VALUES (11, N'Nam', 0)
-INSERT [dbo].[Manafactory] ([ManufactoryId], [Name], [IsDeleted]) VALUES (12, N'Quang', 0)
-INSERT [dbo].[Manafactory] ([ManufactoryId], [Name], [IsDeleted]) VALUES (13, N'Tuan Anh', 0)
-INSERT [dbo].[Manafactory] ([ManufactoryId], [Name], [IsDeleted]) VALUES (14, N'Nhat', 0)
+INSERT [dbo].[Manafactory] ([ManufactoryId], [Name], [IsDeleted]) VALUES (1, N'Asus', 0)
+INSERT [dbo].[Manafactory] ([ManufactoryId], [Name], [IsDeleted]) VALUES (2, N'Acer', 0)
+INSERT [dbo].[Manafactory] ([ManufactoryId], [Name], [IsDeleted]) VALUES (3, N'Dell', 0)
+INSERT [dbo].[Manafactory] ([ManufactoryId], [Name], [IsDeleted]) VALUES (4, N'Razer', 0)
+INSERT [dbo].[Manafactory] ([ManufactoryId], [Name], [IsDeleted]) VALUES (5, N'MSI', 0)
 SET IDENTITY_INSERT [dbo].[Manafactory] OFF
