fb0d9eafd19b364cbc9bfbec1d541e0d3fda97fa
lenhat41555 <batmantrongtinhyeu123@gmail.com>
2020-11-25 20:19:21 +0700

    thiết-kế-get-manufactories

---
+++ PXUK16.API/Database/26112020/DataChanged.sql
@@ -0,0 +1,16 @@
+﻿USE [PXUK16DB]
+GO
+SET IDENTITY_INSERT [dbo].[Manafactory] ON 
+GO
+INSERT [dbo].[Manafactory] ([ManufactoryId], [Name], [IsDeleted]) VALUES (1, N'Mobile', 0)
+GO
+INSERT [dbo].[Manafactory] ([ManufactoryId], [Name], [IsDeleted]) VALUES (2, N'Iphone', 0)
+GO
+INSERT [dbo].[Manafactory] ([ManufactoryId], [Name], [IsDeleted]) VALUES (3, N'SamSum', 0)
+GO
+INSERT [dbo].[Manafactory] ([ManufactoryId], [Name], [IsDeleted]) VALUES (4, N'Nokia', 0)
+GO
+INSERT [dbo].[Manafactory] ([ManufactoryId], [Name], [IsDeleted]) VALUES (5, N'Oppof5', 0)
+GO
+SET IDENTITY_INSERT [dbo].[Manafactory] OFF
+GO
