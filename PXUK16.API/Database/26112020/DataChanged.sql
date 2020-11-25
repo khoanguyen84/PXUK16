4cfe1b47b61427943e9bd75a012bca054b3f2c29
HaHoangK16 <70925614+HaHoangK16@users.noreply.github.com>
2020-11-25 12:02:10 +0700

    Manufactory

---
+++ PXUK16.API/Database/26112020/DataChanged.sql
@@ -0,0 +1,11 @@
+﻿USE [PXUK16DB]
+GO
+SET IDENTITY_INSERT [dbo].[Manafactory] ON 
+
+INSERT [dbo].[Manafactory] ([ManufactoryId], [Name], [IsDeleted]) VALUES (1, N'Apple', 0)
+INSERT [dbo].[Manafactory] ([ManufactoryId], [Name], [IsDeleted]) VALUES (2, N'Nokia', 0)
+INSERT [dbo].[Manafactory] ([ManufactoryId], [Name], [IsDeleted]) VALUES (3, N'Samsung', 0)
+INSERT [dbo].[Manafactory] ([ManufactoryId], [Name], [IsDeleted]) VALUES (4, N'LG', 0)
+INSERT [dbo].[Manafactory] ([ManufactoryId], [Name], [IsDeleted]) VALUES (5, N'Vivo', 0)
+SET IDENTITY_INSERT [dbo].[Manafactory] OFF
+GO
---
+++ PXUK16.API/Database/26112020/Script.sql
@@ -0,0 +1,25 @@
+﻿USE [PXUK16DB]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author:		Minh Hoang
+-- Create date: 25/11/2020
+-- Description:	Get Manafactories have not deleted yet
+-- =============================================
+CREATE PROCEDURE [dbo].[sp_GetManafactory] 
+AS
+BEGIN
+
+	SELECT [ManufactoryId]
+		  ,[Name]
+		  ,[IsDeleted]
+	  FROM [dbo].[Manafactory]
+	  WHERE IsDeleted = 0
+END
+GO
