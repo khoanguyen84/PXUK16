abc7bf8fbcb06bb555474cb7c0f318715701a67d
vanphuoc2205 <74036589+vanphuoc2205@users.noreply.github.com>
2020-11-27 10:21:41 +0700

    h

--- PXUK16.DAL/BaseRepository.cs
+++ PXUK16.DAL/BaseRepository.cs
@@ -11,7 +11,7 @@ namespace PXUK16.DAL
         protected IDbConnection connect;
         public BaseRepository()
         {
-            connect = new SqlConnection(@"Data Source=WIN-CECDPV4A3AJ\SQLEXPRESS;Initial Catalog=PXUK16DB;Integrated Security=True");
+            connect = new SqlConnection(@"Data Source=DESKTOP-5RILCDR;Initial Catalog=PXUK16DB;Integrated Security=True");
         }
     }
 }
