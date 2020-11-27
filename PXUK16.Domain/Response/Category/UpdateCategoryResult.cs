5d628c1603cb54745fa3fe6300977493d59ced16
Administrator <Administrator@WIN-CECDPV4A3AJ>
2020-11-27 10:45:14 +0700

    Fixed

--- PXUK16.Domain/Response/Category/UpdateCategoryResult.cs
+++ PXUK16.Domain/Response/Category/UpdateCategoryResult.cs
@@ -5,7 +5,8 @@ using System.Text;
 namespace PXUK16.Domain.Response.Category
 {
     public class UpdateCategoryResult
-    {
+    {   
+        public int CategoryId { get; set; }
         public string Message { get; set; }
         public string Result { get; set; }
     }
