8b18e69b2b13f8292511f885c4d72c67238e0594
trinhnam12345z <trinhnam123456789z@gmail.com>
2020-11-27 10:46:52 +0700

    Update Category

--- PXUK16.BAL/CategoryService.cs
+++ PXUK16.BAL/CategoryService.cs
@@ -21,10 +21,15 @@ namespace PXUK16.BAL
         {
             return await categoryRepository.CreateCategory(request);
         }
-
+        public async Task<UpdateCategoryResult> UpdateCategory(UpdateCategoryRequest request)
+        {
+            return await categoryRepository.UpdateCategory(request);
+        }
         public async Task<IEnumerable<Category>> Gets()
         {
             return await categoryRepository.Gets();
         }
+
+
     }
 }
--- PXUK16.DAL/CategoryRepository.cs
+++ PXUK16.DAL/CategoryRepository.cs
@@ -28,6 +28,26 @@ namespace PXUK16.DAL
                 throw;
             }
         }
+        
+        public async Task<UpdateCategoryResult> UpdateCategory(UpdateCategoryRequest request)
+        {
+            try
+            {
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("@CategoryId", request.CategoryId);
+                parameters.Add("@CategoryName", request.CategoryName);
+                return await SqlMapper.QueryFirstOrDefaultAsync<UpdateCategoryResult>(cnn: connect,
+                                                    sql: "sp_UpdateCategory",
+                                                    param: parameters,
+                                                    commandType: CommandType.StoredProcedure);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+        }
 
         public async Task<IEnumerable<Category>> Gets()
         {
@@ -37,5 +57,7 @@ namespace PXUK16.DAL
                                                 sql: "sp_GetCategories", 
                                                 commandType: CommandType.StoredProcedure);
         }
+
+        
     }
 }
