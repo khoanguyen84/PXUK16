4f6b831585d6b48863013a2192534f2a4c43df22
quockhanh22 <anhkhanh11761@gmail.com>
2020-11-30 23:03:38 +0700

    khanh

--- PXUK16.Domain/Response/Manafactory/UpdateManafactoryResult.cs
+++ PXUK16.Domain/Response/Manafactory/UpdateManafactoryResult.cs
@@ -6,7 +6,7 @@ namespace PXUK16.Domain.Response.Manafactory
 {
     public class UpdateManafactoryResult
     {
-        public int ManafactoryId { get; set; }
+        public int ManufactoryId { get; set; }
         public string Message { get; set; }
         public string Result { get; set; }
     }
